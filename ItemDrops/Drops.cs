bcee34da57ff33dbc502a353bc6692d3fe8233d8
Pure-Epic <76659728+Pure-Epic@users.noreply.github.com>
2022-07-11 13:07:13 -0400

    Update Drops.cs

--- ItemDrops/Drops.cs
+++ ItemDrops/Drops.cs
@@ -1,1 +1,24 @@
+using Terraria;
+using Terraria.GameContent.ItemDropRules;
 
+namespace TF2.ItemDrops
+{
+	// Very simple drop condition: drop during daytime
+	public class TFDropCondition : IItemDropRuleCondition
+	{
+		public bool CanDrop(DropAttemptInfo info)
+		{	
+			return true;
+		}
+
+		public bool CanShowItemDropInUI()
+		{
+			return true;
+		}
+
+		public string GetConditionDescription()
+		{
+			return "Drops from defeat";
+		}
+	}
+}
